715a14504e8092c027d35c58d37a5a910dc0c3d6
nouf <nouf.aljowaysir@havas.com>
2019-06-26 11:19:02 -0400

    adding vid

--- js/sketch.js
+++ js/sketch.js
@@ -247,3 +247,22 @@ function resize() {
   // resize Styles Section
   stylesSection.style.height = hei + "px";
 }
+
+// // init controller
+// var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+//
+// // build scenes
+// new ScrollMagic.Scene({triggerElement: "#parallax1"})
+// 				.setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+// 				.addIndicators()
+// 				.addTo(controller);
+//
+// new ScrollMagic.Scene({triggerElement: "#parallax2"})
+// 				.setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+// 				.addIndicators()
+// 				.addTo(controller);
+//
+// new ScrollMagic.Scene({triggerElement: "#parallax3"})
+// 				.setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+// 				.addIndicators()
+// 				.addTo(controller);
